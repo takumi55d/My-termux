42fb1a45a7e293a409ab35a32a13991c487006a4
takumi55d <usui55d@gmail.com>
2024-07-04 16:38:57 +0600

    Add files via upload

---
+++ SR-termux.sh
@@ -0,0 +1,21 @@
+#!bin/bash
+#update and upgrade
+pkg update
+pkg install x11-repo root-repo tur-repo -y
+pkg upgrade -
+# fixing the config
+cd
+cd ..
+rm usr/etc/motd
+cd ~/storage/downloads
+cp termux-conf.zip ~
+cd
+rm .bash_history
+unzip termux-conf.zip
+#install python and lolcat
+pkg install python -y
+pip install lolcat
+#install other tools 
+pkg install micro starship figlet fish eza proot-distro -y
+#pd install archlinux
+chsh -s fish
