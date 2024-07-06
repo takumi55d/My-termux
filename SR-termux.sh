beeee174e312d0fb7ee668f26b7828e9850a6f4f
takumi55d <usui55d@gmail.com>
2024-07-07 00:00:17 +0600

    Fix 4 SR-termux.sh

--- SR-termux.sh
+++ SR-termux.sh
@@ -1,19 +1,21 @@
 #!bin/bash
-#update and upgrade
+# update and upgrade
 pkg update
 pkg install x11-repo root-repo tur-repo -y
 pkg upgrade -y
-#install the tools 
-pkg install micro starship figlet fish eza proot-distro -y
-# fixing the config
-cd
+
+# install the tools 
+pkg install micro starship figlet fish eza proot-distro python -y
+#install lolcat
+pip install lolcat
+
+# pd install archlinux
+
+# fixing the config and changing shell to fish 
+cd ~
 cd ..
 rm usr/etc/motd
 curl -O https://raw.githubusercontent.com/takumi55d/My-termux/main/termux-conf.zip
 rm -r .*
 unzip termux-conf.zip
-#install python and lolcat
-pkg install python -y
-pip install lolcat
-#pd install archlinux
 chsh -s fish
